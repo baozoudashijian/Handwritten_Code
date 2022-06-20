64a66019b74d5fd7b81b12caac3803ee6f8773f5
Roger Zhang <1787009521@qq.com>
2022-06-20 10:28:01 +0800

    feat: 完成防抖

--- debounce/index.js
+++ debounce/index.js
@@ -1,1 +1,19 @@
 // 英雄联盟回城
+function debounce(fn, time) {
+    let timer;
+    return () => {
+        if(timer) {
+            clearTimeout(timer)
+        }
+        timer = setTimeout(() => {
+            fn()
+        }, time);
+    }
+
+}
+
+function backHome() {
+    console.log('回城！')
+}
+
+let fn = debounce(backHome, 3000)
