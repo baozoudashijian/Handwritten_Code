d0e589f0a38a393c1bf2bddc730befcfdbaa1e75
Roger Zhang <1787009521@qq.com>
2022-06-28 07:17:40 +0800

    feat: 初步实现EventHub

--- eventHub/index.js
+++ eventHub/index.js
@@ -1,1 +1,22 @@
 // 手写eventHub
+const EventHub = {
+    q: {}, // 选择的数据结构队列
+    on: (type, listener) => {
+        EventHub['q'][type] = EventHub['q'][type] || [] // 防御型编程
+        EventHub['q'][type].push(listener)
+    },
+    emit: (type, data) => {
+        EventHub['q'][type].map((listener) => listener(data))
+    }
+}
+
+EventHub.on('crash', (data) => {
+    console.log(data)
+})
+
+EventHub.on('run', (data) => {
+    console.log(data)
+})
+
+EventHub.emit('crash', 'crash')
+EventHub.emit('run', 'run')
