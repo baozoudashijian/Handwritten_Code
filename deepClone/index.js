d2d10b109ef1c8c34ad4185206b3c8a84544e570
Roger Zhang <1787009521@qq.com>
2022-06-30 21:16:35 +0800

    feat: 实现深拷贝-函数部分

--- deepClone/index.js
+++ deepClone/index.js
@@ -5,9 +5,21 @@ function deepClone() {
         if(a instanceof Function) {
             if(a.prototype) { // 区分箭头函数还是普通函数
                 result = function() { return a.apply(this, arguments) } // 拷贝函数
+            } else { // 箭头函数
+                result = (...args) => { return a.call(undefined, ...args) }
             }
+        }
+        for(let key in a) {
+            result[key] = deepClone(a[key])
         }
     } else {
 
     }
 }
+
+/*
+ * 第一种情况
+ * a = function () {}
+ * a.b = function () {}
+ *
+*/
