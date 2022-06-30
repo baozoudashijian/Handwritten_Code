5000d285f3fd1991e7093c265439848ed5f707ae
Roger Zhang <1787009521@qq.com>
2022-06-30 21:49:04 +0800

    feat: 实现深拷贝-初步实现深克隆

--- deepClone/index.js
+++ deepClone/index.js
@@ -1,4 +1,4 @@
-function deepClone() {
+function deepClone(a) {
     // string number boolean undefined null symbol bigint object
     if(a instanceof Object) { // 分数据类型
         let result = undefined
@@ -8,14 +8,43 @@ function deepClone() {
             } else { // 箭头函数
                 result = (...args) => { return a.call(undefined, ...args) }
             }
+        } else if(a instanceof Array) {
+            result = []
+        } else if(a instanceof Date) {
+            result = new Date(a - 0)
+        } else if(a instanceof RegExp) {
+            result = new RegExp(a.source, a.flags)
+        } else {
+            result = {}
         }
         for(let key in a) {
             result[key] = deepClone(a[key])
         }
+        return result
     } else {
-
+        return a
     }
 }
+
+// 测试代码
+const a = {
+    number: 1,
+    bool: false,
+    str: 'hi',
+    empty1: undefined,
+    empty2: null,
+    array: [
+        {name: 'jack', age: 18},
+        {name: 'ronger', age: 19}
+    ],
+    date: new Date(2000, 0, 1, 20, 30, 0),
+    regex: /\.(j|t)sx/i,
+    object: {name: 'ronger', age: 26},
+    f1: (a, b) => a + b,
+    f2: function(a, b) { return a + b; }
+}
+
+const b = deepClone(a)
 
 /*
  * 第一种情况
